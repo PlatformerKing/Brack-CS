d79b0473fb3fa8839e3685e505e736a96a0b9232
Lockethot <LockeCode@gmail.com>
2018-09-02 17:45:09 -0400

    Alpha 0.2
    * Bugfixed the parser and extension methods.
    * Created a test executable to (finally) showcase what Brack can do!

---
+++ test/Program.cs
@@ -0,0 +1,37 @@
+﻿using Brack.Data;
+using Brack.Data.Operations;
+using Brack.Interpretation;
+using System;
+using System.IO;
+
+namespace Brack_CS_Test
+{
+    class Program
+    {
+        static void Main()
+        {
+            var curRam = new RAM(new OperationSet(new BrackOperatorBase[]{
+                    new BrackParamOperator<float>("add",(r, args) =>
+                    {
+                        var ret = 0f;
+                        foreach(var f in args)
+                        {
+                            ret+=f;
+                        }
+                        return ret;
+                    }),
+                    new BrackOperator<string>("print",(r, str) =>
+                    {
+                        Console.WriteLine(str);
+                        return str;
+                    }),
+                }));
+
+            object[][] brack = BrackParser.ParseString(File.ReadAllText("TestScript.brak"));
+
+            BrackInterpreter.Execute(curRam, brack);
+
+            Console.ReadKey();
+        }
+    }
+}
